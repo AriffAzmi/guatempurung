b42fa9f0ca486161a9e48fb50c65aa87ebaf934d
root <root@vmi111148.contaboserver.net>
2017-04-10 17:26:50 +0200

    add new function

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class User extends Model
 {
@@ -12,9 +13,9 @@ class User extends Model
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    // protected $fillable = [
+    //     'name', 'email', 'password',
+    // ];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -26,5 +27,46 @@ class User extends Model
     ];
 
     protected $table="users";
+
+
+    public function ifExist($data=null)
+    {
+        $check = DB::select("
+            select 
+                count(*) as total
+            from 
+                users 
+            where 
+                name=?
+                OR
+                email=?
+                OR
+                password=?
+            limit 1", 
+            [$data,$data,$data]
+        );
+
+        return $check;
+    }
+
+    // public function find()
+    // {
+    //     $object = get_object_vars($this);
+
+    //     $clause = "";
+    //     $params= "";
+
+    //     foreach ($object['attributes'] as $key => $value) {
+            
+    //         $clause.="$key=? AND ";
+    //         $params[] = $value;
+    //     }
+
+    //     $clause = rtrim($clause,"AND ");
+
+    //     $result = DB::select("select * from users where $clause", $params);
+
+    //     return $result;
+    // }
     
 }
